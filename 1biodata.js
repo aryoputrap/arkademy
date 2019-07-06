09038881b9ee18aed7727c6a196a84794e93445b
Aryo Putra Purwanto <42566360+aryoputrap@users.noreply.github.com>
2019-07-06 18:48:42 +0700

    Add files via upload

---
+++ 1biodata.js
@@ -0,0 +1,26 @@
+function myBiodata(biodata){
+  let biodataObj = {
+    "name": 'Aryo Putra PUrwanto',
+    "age": 24,
+    "address":'Jl.Raya Bogor KM 46, Cibinong, Kab.Bogor'
+    "hobbies": ['Futsal', 'Jogging', 'Game', 'Traveling','Koding'],
+    "is_married": false,
+    "list_school":[
+       {
+      "university": 'ITB',
+      "year_in":2012, 
+      "year_out" : 2016,
+      "major" : 'Biology'
+       }],
+    "skills": [
+      {
+        "Android": 'beginner',
+        "Java": 'beginner',
+        "Web": 'beginner',
+      }
+    ]
+  }
+
+  return biodataObj
+}
+console.log(myBiodata('ari'));
